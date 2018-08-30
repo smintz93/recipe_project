c647addd46c4826fc209b6c43bcb7f74c239f8be
Sam <sammintz1993@gmail.com>
2018-08-30 11:10:40 -0500

    simple config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// built in node module 
+const path = require('path')
+
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist/js'),
+		filename: 'bundle.js'
+	},
+	mode: 'development'
+};
