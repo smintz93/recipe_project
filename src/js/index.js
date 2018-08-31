ac7389c54e810f93e1c229047e66d2edc8927be0
Sam <sammintz1993@gmail.com>
2018-08-31 10:50:03 -0500

    babel-polyfill installed

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import num from './test.js'
+const x = 23;
 
-console.log(`I imported ${num} from another Test.js`);
+console.log(`I imported ${num} from another Test.js. Variable ${x} is x`);
