837c49045569f400fe2024f05e3b62bf43a56c5a
Sam <sammintz1993@gmail.com>
2018-08-31 10:19:46 -0500

    installed babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/js/index.js',
+	entry: ['babel-polyfill','./src/js/index.js']
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'js/bundle.js'
@@ -16,5 +16,16 @@ module.exports = {
 			filename: 'index.html',
 			template: './src/index.html'
 		})
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/, // look for all js files and test if they end with .js
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
