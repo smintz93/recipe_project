c51e31d01dd678aabfd4012cd74ed1a76629e851
Sam <sammintz1993@gmail.com>
2018-09-04 11:32:55 -0500

    made api request in console

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
+
 //  http://food2fork.com/api/search
 
 // 592d87178e875f403f816544a1cf57cb
 
 async function getResults(query) {
+	const proxy = 'https://cors-anywhere.herokuapp.com/'
+	const key = '592d87178e875f403f816544a1cf57cb'
+	const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+	console.log(res)
 
-	
 }
 
-getResults();
+getResults('pizza');
