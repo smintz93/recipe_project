fa6f5ba8d1a86bd0328dbf5cadadcded7afb5469
Sam <sammintz1993@gmail.com>
2018-09-04 11:39:15 -0500

    error handling for api calls

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,13 @@ import axios from 'axios';
 async function getResults(query) {
 	const proxy = 'https://cors-anywhere.herokuapp.com/'
 	const key = '592d87178e875f403f816544a1cf57cb'
-	const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-	console.log(res)
+	try {
+		const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+		const recipes = res.data.recipes;
+		console.log(recipes)
+	} catch (error){
+		alert(error);
+	}
 
 }
 
