19fcd9007986bfd09cb102c59e57665ee3311389
Sam <sammintz1993@gmail.com>
2018-09-05 13:05:19 -0500

    this.result stores recipe data

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search'
 
-//  http://food2fork.com/api/search
-
-// 592d87178e875f403f816544a1cf57cb
-
-async function getResults(query) {
-	const proxy = 'https://cors-anywhere.herokuapp.com/'
-	const key = '592d87178e875f403f816544a1cf57cb'
-	try {
-		const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-		const recipes = res.data.recipes;
-		console.log(recipes)
-	} catch (error){
-		alert(error);
-	}
-
-}
-
-getResults('pizza');
+const search = new Search('pizza')
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,9 +9,9 @@ export default class Search {
 		const proxy = 'https://cors-anywhere.herokuapp.com/'
 		const key = '592d87178e875f403f816544a1cf57cb'
 		try {
-			const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-			const recipes = res.data.recipes;
-			console.log(recipes)
+			const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+			this.result = res.data.recipes;
+			console.log(this.result)
 		} catch (error){
 			alert(error);
 		}
