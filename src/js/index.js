f4b712ae2580d50f1f9ccf3fccf68b1ded16f0c8
Sam <sammintz1993@gmail.com>
2018-09-05 13:20:51 -0500

    data return with search btn

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,33 @@
 import Search from './models/Search'
 
-const search = new Search('pizza')
-console.log(search);
-search.getResults();
+// GLOBAL STATE OF APP 
+// - Search object
+// Current recipe object
+// Shopping list object
+// Liked recipes 
+const state = {}
+
+const controlSearch = async () => {
+	// get the query from view 
+	const query = 'pizza' // TODO LATER
+
+	if(query) {
+		// new search obkect and add it to state 
+		state.search = new Search(query)
+
+		// Prepare UI for results 
+
+		// Search for recipe 
+		await state.search.getResults();
+
+		// render results on UI 
+
+	}
+}	
+
+document.querySelector('.search').addEventListener('submit', e => {
+	e.preventDefault()
+	controlSearch();
+})
+
+
