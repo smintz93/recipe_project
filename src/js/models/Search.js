140852739a00c6f1bc6fd2b6c0744714118ea2e1
Sam <sammintz1993@gmail.com>
2018-09-05 13:00:59 -0500

    moved api call to Search.js

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string'
+import axios from 'axios';
+
+export default class Search {
+	constructor(query){
+		this.query = query;
+	}
+
+	async getResults(query) {
+		const proxy = 'https://cors-anywhere.herokuapp.com/'
+		const key = '592d87178e875f403f816544a1cf57cb'
+		try {
+			const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+			const recipes = res.data.recipes;
+			console.log(recipes)
+		} catch (error){
+			alert(error);
+		}
+
+	}
+
+}
+
